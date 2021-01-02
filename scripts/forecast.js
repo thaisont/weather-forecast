9e2e1838b7f3efa1be7c473aef9372e0864dc26f
Thai Truong <sontruong@hotmail.co.uk>
2021-01-02 01:22:49 +0000

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,33 @@
+const key = "wCZPL52hkpD0N18e0myn9Ti8DHQzKEhx";
+
+// Get weather information
+
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// Get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// getCity("manchester")
+//   .then((data) => {
+//     return getWeather(data.Key);
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.log(err));
